dd6738d5310995ea0b7e31ffbda728d9faf935af
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-05-03 08:50:49 +0600

    Object types practice

---
+++ module1/src/1.5_Object_optional_Literal_types.ts
@@ -0,0 +1,13 @@
+//Object types
+
+const user : {
+    address: 'shekhertek 2'; //--------> Literal Types
+    firstNmae: string;
+    middleName?: string; //------------> Optional tpes
+    lastName: string;
+} = {
+    address: 'shekhertek 2',
+    firstNmae: 'Mushfiqur',
+    middleName: "Rahman",
+    lastName:'Badhon',
+}
