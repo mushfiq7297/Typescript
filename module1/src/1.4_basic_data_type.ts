f0400cd1f56fc69567a8b6cb8f37c46dd035a761
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-05-03 08:28:58 +0600

    first commit

---
+++ module1/src/1.4_basic_data_type.ts
@@ -0,0 +1,19 @@
+//Basic Data Type
+
+//String
+let FirstNamr:string = "Mushfiq"
+
+//Number
+let id : number = 1710921178
+
+//Array
+
+let friends: string[] = ['shuhad','saju']
+
+friends.push('robin')
+
+//tuple
+
+//array--> tuple--> order--> types of values
+
+let ageName : [number,string] = [22,'mr.x']
---
+++ module1/src/index.ts
@@ -0,0 +1,2 @@
+let course : string = "i am a programmer"
+console.log(course)
